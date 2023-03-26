ce3ec8e13ed1b8d3e127c146d77c496d1cc0cd44
neerajpandit <pandeyneeraj1507@gmail.com>
2023-03-26 11:05:50 +0530

    sh

--- buversity/settings.py
+++ buversity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mk*z!ey4mkc!0)p8lr*m*!vnmf)a09bp5tblnl*4a+s9t$=h&z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,15 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_URL = '/static/'
-STATICFILES_DIR=[BASE_DIR/'static']
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/'media'
-
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
